0a44a94178c082048e47512ef6bab0e313fa689d
Nazwa8 <matiskowron2006@gmail.com>
2025-05-12 21:28:52 +0200

    Skonczone

--- app/src/main/java/com/example/zadanie8/MainActivity.java
+++ app/src/main/java/com/example/zadanie8/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.zadanie8;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
     TextView emailTekst;
     TextView hasloTekst;
     Button przycisk;
-    TextView okno;
+    TextView komunikat;
+    TextView imie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +33,114 @@ public class MainActivity extends AppCompatActivity {
         emailTekst = findViewById(R.id.email);
         hasloTekst = findViewById(R.id.haslo);
         przycisk = findViewById(R.id.przycisk);
-        okno = findViewById(R.id.oknoDialogowe);
+        komunikat = findViewById(R.id.komunikatPole);
+        imie = findViewById(R.id.imie);
 
-        okno.setTextColor(Color.GRAY);
-        okno.setText("Witaj! Aplikacja wykonana przez: Mateusz Skowron");
+        komunikat.setTextColor(Color.GRAY);
+        komunikat.setText("Witaj! Aplikacja wykonana przez: Mateusz Skowron");
+        imie.setText("Mateusz Skowron 4TP");
 
         przycisk.setOnClickListener(v -> {
-            edycjaprofilu();
+            showCustomDialog();
+            emailTekst.setText("");
+            hasloTekst.setText("");
         });
     }
 
-    public void edycjaprofilu(){
 
-        TextView podajNoweHasloNapis = new TextView(this);
-        podajNoweHasloNapis.setText("Podaj nowe hasło: ");
-        TextView powtorzoneHasloNapis = new TextView(this);
-        powtorzoneHasloNapis.setText("Powtórz hasło: ");
+    private void showCustomDialog(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_dialog);
 
-        EditText nowyEmail = new EditText(this);
-        EditText noweHaslo = new EditText(this);
-        EditText powNoweHaslo = new EditText(this);
+        EditText wpisantEmail = dialog.findViewById(R.id.nowyEmail);
+        Button btnSubmit = dialog.findViewById(R.id.btnSubmitEmail);
+        Button btnCancel = dialog.findViewById(R.id.btnCancel);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputEmail = wpisantEmail.getText().toString().trim(); // trim() usuwa białe znaki
+
+                if (!inputEmail.contains("@")){
+                    dialog.dismiss();
+                    komunikat.setTextColor(Color.RED);
+                    komunikat.setText("Błąd: Nieprawidłowy format emaila.");
+                }else{
+                    dialog.dismiss();
+                    showCustomDialog2();
+                    emailTekst.setText(inputEmail);
+                    komunikat.setTextColor(Color.GREEN);
+                    komunikat.setText("Profil zaktualizowany! Nowy email: " + inputEmail);
+                }
+            }
+        });
 
 
 
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // zamykanie
+                komunikat.setTextColor(Color.GRAY);
+                komunikat.setText("Edycja profilu anulowana.");
+                dialog.dismiss();
+            }
+        });
+
+            dialog.show();
+
+         Window window = dialog.getWindow();
+         if (window != null) {
+             window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+         }
     }
+
+
+    private void showCustomDialog2(){
+        final Dialog dialog2 = new Dialog(this);
+        dialog2.setContentView(R.layout.custom_dialog_password);
+
+        EditText wpisaneHaslo = dialog2.findViewById(R.id.noweHaslo);
+        EditText powHaslo = dialog2.findViewById(R.id.powNowe_Haslo);
+        Button btnSubmit2 = dialog2.findViewById(R.id.btnSubmitPassword);
+        Button btnCancel2 = dialog2.findViewById(R.id.btnCancel2);
+
+        //zapisz
+        btnSubmit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputPassword = wpisaneHaslo.getText().toString().trim(); // trim() usuwa białe znaki
+                String repeatPassword = powHaslo.getText().toString().trim();
+                if (!inputPassword.equals(repeatPassword)){
+                    dialog2.dismiss();
+                    komunikat.setTextColor(Color.RED);
+                    komunikat.setText("Błąd: Hasła nie pasują do siebie.");
+                }else{
+                    dialog2.dismiss();
+                    hasloTekst.setText(inputPassword);
+
+                }
+            }
+        });
+
+        //anuluj
+        btnCancel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // zamykanie
+                komunikat.setTextColor(Color.GRAY);
+                komunikat.setText("Edycja profilu anulowana.");
+                dialog2.dismiss();
+            }
+        });
+
+        dialog2.show();
+
+         Window window = dialog2.getWindow();
+         if (window != null) {
+             window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+         }
+
+    } // koniec dialogu 2
+
 }
